d1d3b6827ae295575125a9480a341bd158d2a459
정핸모 <gusah009@naver.com>
2023-05-02 09:42:57 +0900

    gusah009.github.io로 커스터마이징

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import feedparser, time
+import requests
 
-URL="https://jojaeng2.tistory.com/rss" 
-RSS_FEED = feedparser.parse(URL)
+URL = "https://gusah009.github.io"
+DATA = requests.get(URL + "/page-data/index/page-data.json").json()["result"]["data"]["allMarkdownRemark"]["edges"]
 
 markdown_text = """
-
-markdown_text += ![image](https://user-images.githubusercontent.com/76645095/162124599-f9d701d6-e523-49c4-a6ce-193dc38f1026.png)
-
 ## ✅ Latest Blog Post
 
 """  # list of blog posts will be appended here
 
 MAX_POST = 5
-for idx, feed in enumerate(RSS_FEED['entries']):
+for idx, feed in enumerate(DATA):
     if idx > MAX_POST:
         break
     else:
-        feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-        
+        print(feed)
+        feed_path = feed["node"]["fields"]["slug"]
+        feed_date = feed["node"]["frontmatter"]["date"][:-1].replace(".", "-")
+        feed_title = feed["node"]["frontmatter"]["title"]
+        print(feed_path, feed_date, feed_title)
+        markdown_text += (
+            f"[{feed_date} - {feed_title}]({URL + feed_path}) <br/>\n".replace("", "")
+        )
+
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()
